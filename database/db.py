a72d8fcffff50c31afefc8666aa83bbeda85829f
Jean <jp_9912@hotmail.com>
2026-06-20 11:30:58 -0500

    ralway

--- database/db.py
+++ database/db.py
@@ -1,11 +1,23 @@
 import mysql.connector
 
+# def obtener_conexion():
+
+#     conexion = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="admin",
+#         database="alimentos_iot"
+#     )
+
+#     return conexion
+
 def obtener_conexion():
 
     conexion = mysql.connector.connect(
-        host="localhost",
+        host="turntable.proxy.rlwy.net",
+        port=45347,
         user="root",
-        password="admin",
+        password="zQFeKipPVXmlGBTsqJATKJfxlmOcKYtV",
         database="alimentos_iot"
     )
 
